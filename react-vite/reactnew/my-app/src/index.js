8c2bebfd8602b520acd3d0f76cd94ab243940c8c
Ainal <ainal1haque@gmail.com>
2023-07-24 23:52:20 +0600

    styling with react: inline,internal external css

--- react-vite/reactnew/my-app/src/index.js
+++ react-vite/reactnew/my-app/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css'
 
 const todoTitle="Hello World";
 const todoDescription="Hello World";
@@ -7,11 +8,26 @@ const date=new Date();
 const curentYear=date.getFullYear();
 const dateName=date.getDate();
 const dateMonth=date.getMonth();
+const headingStyle={
+  backgroundColor:"purple",
+  color:"white",
+  textAlign:"center",
+  padding:"15px",
+  fontSize:"16px"
+
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<div>
-  <h1>To do App</h1>
-  <h3>{todoTitle}</h3>
-  <p>{todoDescription}</p>
-  <p>{dateName+'/'+dateMonth+'/'+curentYear}</p>
+  <h1 style={headingStyle}>To do App</h1>
+  <h3 className='secondheadingStyle'>{todoTitle}</h3>
+  <p className='paragraphStyle'>{todoDescription}</p>
+  <p style={{
+  backgroundColor:"purple",
+  color:"white",
+  textAlign:"center",
+  padding:"15px",
+  fontSize:"16px"
+
+}}>{dateName+'/'+dateMonth+'/'+curentYear}</p>
   </div>);
 
