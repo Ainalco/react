6f9f0f8853d6e3232c8f17b2084f38e5e3a1db50
Ainal <ainal1haque@gmail.com>
2023-07-26 21:58:18 +0600

    component mapping

--- react-vite/reactnew/my-app/src/App.js
+++ react-vite/reactnew/my-app/src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 // import Stylerule from './components/Stylerule';
 import Card from './components/Card';
+import Data from './data.json';
 
+//let items=[];
+// for(let x=0;x<Data.length;x++){
+//     items.push(<Card titleTxt={Data[x].title} descText={Data[x].desc} />)
+// }
+
+//for mapping
+// items =Data.map((item) => <Card titleTxt={item.title} descText={item.desc} />);
 
 function App(){
     return <div><h1 className='secondheadingStyle'>To do App</h1>
             {/* <Stylerule /> */}
-            <Card titleTxt="Call Mother" descText="Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." />
-            <Card titleTxt="Call Father" descText="Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." />
-            <Card titleTxt="Call Brother" descText="Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." />
-            <Card titleTxt="Call Sister" descText="Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." />
-
+           {/* {items} */}
+           {Data.map((item, index) => <Card key={index} titleTxt={item.title} descText={item.desc} />)};
     </div>
 
 }
