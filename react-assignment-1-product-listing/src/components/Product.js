7a048e38cf41c94802f9c0d26ababb70e5a631a4
Ainal <ainal1haque@gmail.com>
2023-07-30 23:48:05 +0600

    react project 1 Assignment1

---
+++ react-assignment-1-product-listing/src/components/Product.js
@@ -0,0 +1,21 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+const Product = (product) => {
+
+  return (
+      <article className="product">
+      <img src={product.product.image} alt="image" />
+      <div className="product__details">
+        <h4 className="product__title">{product.product.title}</h4>
+        <p>Price: $ {product.product.price}</p>
+        <p>Rating: {product.product.rating.rate}</p>
+        <p className="product__desc">{product.product.description}</p>
+        <button className="product__btn btn">Add to cart</button>
+      </div>
+    </article>
+  );
+};
+
+export default Product;
