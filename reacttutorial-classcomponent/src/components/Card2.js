77fd764e1d619e4e3a09f56a2f18bf95a81cf7a2
Ainal <ainal1haque@gmail.com>
2023-08-02 23:49:58 +0600

    class component va functional component

---
+++ reacttutorial-classcomponent/src/components/Card2.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+class Card2 extends Component{
+        render(){
+            return(
+                <h3>{this.props.name}</h3>
+            )
+        }
+}
+
+export default Card2;
