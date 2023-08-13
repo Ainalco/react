52dea22b496e88532827c3c016e6f8a901ad0084
Ainal <ainal1haque@gmail.com>
2023-08-13 18:41:42 +0600

    project 1 Todo App stylying

--- project1/src/components/Todo.js
+++ project1/src/components/Todo.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import style from './Todo.module.css';
 
 const Todo = (props) => {
     const {title, desc,id}=props.todo;
   return (
-    <article>
+    <article className={style.todo}>
       <div>
         <h1>{title}</h1>
         <p>{desc}</p>
       </div>
       <div>
-        <button><i className='fa fa-trash fa-2x'></i></button>
+        <button className={style.btn}><i className='fa fa-trash fa-2x'></i></button>
       </div>
     </article>
   )
--- project1/src/components/Todos.js
+++ project1/src/components/Todos.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Todo from './Todo';
+import style from './Todos.module.css';
 
 const Todos = (props) => {
     console.log(props.todos)
   return (
-    <section>
+    <section className={style.todos}>
     {
        props.todos.map((todo)=>(<Todo todo={todo} key={todo.id}/>)) 
     }
