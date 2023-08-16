3f4d04c3e36b4c783966acc58b9b64e1dbb4f548
Ainal <ainal1haque@gmail.com>
2023-08-16 22:17:59 +0600

    toggle with Faqs

--- reacttutorial-toggle/src/components/FAQ/Faqs.js
+++ reacttutorial-toggle/src/components/FAQ/Faqs.js
@@ -1,14 +1,15 @@
 import React,{useState} from 'react';
+import style from './faqs.module.css';
 import { faqsData } from './data';
 import Faq from './Faq';
 
 const Faqs = () => {
   const [faqs, setFaqs]=useState(faqsData);
   return (
-    <main>
-      <section>
+    <main className={style.container}>
+      <section className={style.faqs}>
         <h1>Faqs</h1>
-        {faqs.map(faq => <Faq key={faq.id}  {...faq}/>)}
+        {faqs.map((faq) => (<Faq key={faq.id}  {...faq}/>))}
       </section>
     </main>
   )
