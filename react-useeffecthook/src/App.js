9d58ba9cdfd9a5365f9054bef600243623385e58
Ainal <ainal1haque@gmail.com>
2023-08-17 23:40:02 +0600

    start useEffect with fetch api data

--- react-useeffecthook/src/App.js
+++ react-useeffecthook/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import UseEffect from './components/useEffect/UseEffect';
+import DataFetch from './components/useEffect/DataFetch';
 
 export default function App() {
   return (
     <div>
       <UseEffect />
+      <DataFetch />
     </div>
   );
 }
