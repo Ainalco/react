d52a7b7f0ef5cf45270293e00255a9bb09d0c29d
Ainal <ainal1haque@gmail.com>
2023-08-19 00:09:10 +0600

    Done Assignment 3

--- react-assignment-4/src/components/Users.js
+++ react-assignment-4/src/components/Users.js
@@ -1,33 +1,18 @@
-/* eslint-disable prettier/prettier */
-/* eslint no-unused-vars : "off" */
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-//import User from './User';
+/* eslint-disable prettier/prettier */
+/* eslint no-unused-vars : "off" */
+import User from './User';
 
-const Users = (props) => {  
-  //const allusers=props.users;
-  console.log(props);
-  const listuser=props.users.map((user) =>{
-    return <article className="user" key={user.id}>
-    <h3>{user.id}</h3>
-    <h3 className="user__name">{user.name}</h3>
-    <p className="user__email">{user.email}</p>
-    <a className="user__phone" href={'tel:+' + user.phone}>
-      {user.phone}
-    </a>
-  </article>
-  });
-  
+const Users = (props) => {
   return (
 
-    <section className="users">
-      {listuser}
-      
-      {/* {props.users.map((user) => (
+    <section className="users">       
+      {props.users.map((user) => (
         <User key={user.id} {...user} />
-      ))}      */}
-      Hello
+      ))}     
+      
     </section>
     
   );
