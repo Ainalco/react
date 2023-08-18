eda26e14c64992d095c4df469078a3ebcba6b631
Ainal <ainal1haque@gmail.com>
2023-08-18 19:30:53 +0600

    start useEffect with fetch api datahandiling and error

--- react-useeffecthook/src/components/useEffect/DataFetch.js
+++ react-useeffecthook/src/components/useEffect/DataFetch.js
@@ -1,24 +1,41 @@
 import React, { useEffect, useState } from 'react'
-
+const loadingMessage = <p>Todo is Loading</p>;
 const DataFetch = () => {
-    const [todos,setTodos]=useState(null)
-    useEffect(()=>{        
-            fetch("https://jsonplaceholder.typicode.com/todos")
-        .then((res)=>{
-            return res.json()
-        })
-        .then((data)=>{
-            setTodos(data);
-            //console.log(todos);
-        });
-       
-    }, []);
+  const [todos, setTodos] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("Fetching Not Okay")
+        } else {
+          return res.json()
+        }
+      })
+      .then((data) => {
+        setTodos(data);
+        setIsLoading(false);
+        setError(null);
+        //console.log(todos);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setIsLoading(false);
+      });
+
+  }, []);
+  const todoselement = todos &&
+    todos.map((todo) => {
+      return <p key={todo.id}>{todo.title}</p>;
+    });
+
   return (
     <div>
       <h1>Todos</h1>
-      {todos && todos.map((todo)=>{
-            return <p key={todo.id}>{todo.title}</p>;
-      })}
+      {error && <p>{error}</p>}
+      {isLoading && loadingMessage}
+      {todoselement}
 
     </div>
   )
