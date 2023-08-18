a164549afaaee5d1da1df269af89778b49629175
Ainal <ainal1haque@gmail.com>
2023-08-18 20:15:41 +0600

    use react Tostify plugin

--- react-useeffecthook/src/App.js
+++ react-useeffecthook/src/App.js
@@ -3,12 +3,24 @@ import UseEffect from './components/useEffect/UseEffect';
 import DataFetch from './components/useEffect/DataFetch';
 import CustomDataFetch from './components/useEffect/CustomDatafetch';
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 export default function App() {
+  const handleAddTodo=()=>{
+    toast("New Todo is Added");
+  }
+  const handleDeleteTodo=()=>{
+    toast("Todo Is Deleted!!");
+  }
   return (
     <div>
       <UseEffect />
       <DataFetch />
       <CustomDataFetch />
+      <button onClick={handleAddTodo}>Add New Todo</button>
+      <button onClick={handleDeleteTodo}>Delete Todo</button>
+      <ToastContainer />
     </div>
   );
 }
