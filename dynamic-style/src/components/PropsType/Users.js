03e0faae236a1c8214d943ae3206d6c2c81edbe4
Ainal <ainal1haque@gmail.com>
2023-08-22 22:31:20 +0600

    use Lifecycle class component

--- dynamic-style/src/components/PropsType/Users.js
+++ dynamic-style/src/components/PropsType/Users.js
@@ -1,13 +1,17 @@
-import React, { useState } from 'react';
-import User from './User';
+import React, { useState } from "react";
+import User from "./User";
 
 const Users = () => {
-    const [user, setUser]=useState({id:123456,name:"Ainal"});
+  const [user, setUser] = useState({
+    id: 1302020017,
+    name: "anisul islam",
+  });
+
   return (
     <>
-      <User user={user}/>
+      <User user={user} />
     </>
-  )
-}
+  );
+};
 
-export default Users
+export default Users;
