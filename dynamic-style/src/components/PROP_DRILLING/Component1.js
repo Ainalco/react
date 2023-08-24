fe8db59a878aea5768e29bb8f65e9060e901ac1b
Ainal <ainal1haque@gmail.com>
2023-08-24 21:41:12 +0600

    Use Usercontext

--- dynamic-style/src/components/PROP_DRILLING/Component1.js
+++ dynamic-style/src/components/PROP_DRILLING/Component1.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import Component2 from './Component2';
+import { UserContext } from './UserContext';;
 
 const Component1 = () => {
     const [user,setUser]=useState({id:101,name:"Ainal"});
   return (   
     <div>
+        <UserContext />
       <Component2 user={user}/>
     </div>
   )
