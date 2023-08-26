4e5e0ac2af1e7b969463b41917507d886a0ec38c
Ainal <ainal1haque@gmail.com>
2023-08-26 22:17:52 +0600

    User react router

---
+++ react-router/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import About from './pages/About';
+import Blog from './pages/Blog';
+
+function App() { 
+  return (   
+    <div>
+      
+    </div>
+    
+  );
+}
+
+export default App;
+
+
