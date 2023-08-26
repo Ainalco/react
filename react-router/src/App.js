44318bef73ebe2b88789f404731103e4043cce0b
Ainal <ainal1haque@gmail.com>
2023-08-26 22:28:38 +0600

    User react router add with routing plugin

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import About from './pages/About';
 import Blog from './pages/Blog';
+import Error from './pages/Error';
+
+import "./App.css";
 
 function App() { 
   return (   
-    <div>
-      
-    </div>
+    <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/blog' element={<Blog />} />
+            <Route path='*' element={<Error />} />
+        </Routes>
+    </BrowserRouter>
     
   );
 }
