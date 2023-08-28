f4b931a68cbf4660b20fc0b7cf555da729affd82
Ainal <ainal1haque@gmail.com>
2023-08-28 20:34:22 +0600

    get data from local json server using axios

--- react-json-server-create/src/App.js
+++ react-json-server-create/src/App.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
 
 function App() {
+  const [users, setUsers]=useState([]);
+  
+  const getAllUsers = async ()=>{
+    const response = await axios.get("http://localhost:3001/users");
+    setUsers(response.data);
+  }
+
+  useEffect(()=>{
+    getAllUsers();
+  },[])
+
   return (
     <div className="App">
-      Hello
+      {users.map(user =>{
+        return (
+          <article key={user.id}>
+            <h2>{user.name}</h2>
+            <p>{user.email}</p>
+          </article>
+        )
+      })}
     </div>
   );
 }
